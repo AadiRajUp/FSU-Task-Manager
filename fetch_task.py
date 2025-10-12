88c803952c30080045dee00dee0e77323368ca5b
Aaditya Raj Uprety <aadityaru6407@gmail.com>
2025-10-12 13:41:38 +0545

    bug fix

--- fetch_task.py
+++ fetch_task.py
@@ -2,7 +2,7 @@ from flask import session
 from flask import Blueprint
 from connect import connectDB
 from connect import DB_NAME,TASK_ID_TABLE, USER_ID_TABLE
-def fetchAll(taskType,sortBy):
+def fetchAll(taskType="all",sortBy="task_deadline"):
     conn = connectDB()
     cur = conn.cursor()
     query1= f"""
